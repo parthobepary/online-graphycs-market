a5a1cf25c53ae7d69a4d9d39657fcd463c19272a
pallobbiswas <pallobbiswas326@gmail.com>
2022-06-22 15:23:23 +0600

    banner added

--- src/pages/Js/Banner.js
+++ src/pages/Js/Banner.js
@@ -1,10 +1,28 @@
+import cover from "../../assets/cover.jpeg";
+import '../Css/banner.css';
 
 const Banner = () => {
-    return (
-        <div id="home">
-            
+  return (
+    <div id="home" className="banner-height">
+      <div
+        class="hero min-h-screen banner-height"
+        style={{ backgroundImage: `url(${cover})`}}
+      >
+        <div class="hero-overlay bg-opacity-40"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-md">
+            <h1 class="mb-5 text-2xl font-bold">Hello there</h1>
+            <p class="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button class="btn btn-primary">Go to facebook page</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
